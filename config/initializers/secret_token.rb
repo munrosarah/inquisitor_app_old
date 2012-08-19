da2891a1aa0588e734f25bc30efc0d2463592f5b
Sarah Munro <munro.sarah@gmail.com>
2012-08-19 10:39:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InquisitorApp::Application.config.secret_token = '066b85274426bea39c4b88c20850667687de96087ae46f0fb703cef42ee66ba46b3daa7914bb9927459e423800ecf3593a988d943ef149b666b8114378d6a88b'
